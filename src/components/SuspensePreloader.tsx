7769a719f64d01be71a1490810e5dad7426f819e
Vlad Vylotnilov <vladvyljotnikov@gmail.com>
2022-08-08 15:21:29 +0300

    use lazy for Cart component

---
+++ src/components/SuspensePreloader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SuspensePreloader: React.FC = () => (
+    <div style={{ margin: '20px' }}>
+        <h2>Загрузка страницы...</h2>
+    </div>
+)
+
+export default SuspensePreloader
--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import SuspensePreloader from '../components/SuspensePreloader'
 
 import MainLayout from '../layouts/MainLayout'
-import Cart from './Cart'
 import Home from './Home'
 import NotFound from './NotFound'
 
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ './Cart'))
+
 const MyRoutes: React.FC = () => (
-    <Routes>
-        <Route path="/" element={<MainLayout />}>
-            <Route path="" element={<Home />} />
-            <Route path="cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-        </Route>
-    </Routes>
+    <React.Suspense fallback={<SuspensePreloader />}>
+        <Routes>
+            <Route path="/" element={<MainLayout />}>
+                <Route path="" element={<Home />} />
+                <Route path="cart" element={<Cart />} />
+                <Route path="*" element={<NotFound />} />
+            </Route>
+        </Routes>
+    </React.Suspense>
 )
 
 export default MyRoutes
